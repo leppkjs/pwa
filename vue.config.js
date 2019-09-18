80a190c34ccdd1b9b56e6f21e005f682c4839251
김재섭B <jaeseopkim@smilegate.com>
2019-09-18 14:47:41 +0900

    pwa test

---
+++ vue.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const fs = require('fs');
+const resolve = dir => path.join(__dirname, dir);
+
+const config = {
+    pluginOptions: {
+        i18n: {
+            localeDir: 'locales',
+            enableInSFC: true
+        },
+        moment: {
+            locales: ['ko', 'ja', 'zh-tw', 'zh-cn', 'de', 'en', 'es', 'fr', 'pt']
+        }
+    },
+    devServer: {
+        // http2: true,
+        https: true,
+        // https: {
+        //     key: fs.readFileSync('cert/star.onstove.com/onstove_private.key'),
+        //     cert: fs.readFileSync('cert/star.onstove.com/onstove_private.crt'),
+        //     ca: fs.readFileSync('cert/star.onstove.com/onstove_private.pem')
+        // },
+        disableHostCheck: true,
+        compress: true,
+        port: 9000,
+        allowedHosts: [
+            'localhost',
+            'onstove.com'
+        ]
+    },
+    productionSourceMap: false,
+    pwa: {
+        name: 'Stove'
+    }
+};
+
+module.exports = config;
