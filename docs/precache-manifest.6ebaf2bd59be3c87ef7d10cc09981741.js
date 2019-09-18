0273870fa0a2f5b186e5fad4b2d8a8cb18ef8f01
김재섭B <jaeseopkim@smilegate.com>
2019-09-18 17:21:59 +0900

    pwa docs

---
+++ docs/precache-manifest.6ebaf2bd59be3c87ef7d10cc09981741.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2246ac5e0172b29dd84c",
+    "url": "/js/about.a97d7ee6.js"
+  },
+  {
+    "revision": "410e076e4334514e7b58",
+    "url": "/css/app.f1513510.css"
+  },
+  {
+    "revision": "410e076e4334514e7b58",
+    "url": "/js/app.d6bf804a.js"
+  },
+  {
+    "revision": "f5b7ebc128188f68b363",
+    "url": "/js/chunk-vendors.f50406ee.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2b52387af57fc5cb30d278a211687db4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
